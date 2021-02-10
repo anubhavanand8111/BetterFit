4c703191818aa9635f178ca04abd0e12954eb765
anubhavanand8111 <anubhavanand8111@gmail.com>
2021-02-10 18:23:22 +0530

    added Exercise Activity and Toolbar

---
+++ app/src/main/java/com/example/betterfit/ExerciseActivity.kt
@@ -0,0 +1,22 @@
+package com.example.betterfit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class ExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+        val toolbar = findViewById<Toolbar>(R.id.exercise_activity_toolbar)
+
+        setSupportActionBar(toolbar)
+        val actionbar=supportActionBar
+        if (actionbar!=null){
+            actionbar.setDisplayHomeAsUpEnabled(true)
+        }
+        toolbar.setNavigationOnClickListener {
+            onBackPressed( )
+        }
+    }
+}
--- app/src/main/java/com/example/betterfit/MainActivity.kt
+++ app/src/main/java/com/example/betterfit/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.betterfit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn = findViewById<View>(R.id.ll_start_btn)
+        startBtn.setOnClickListener {
+            val i =Intent(this,ExerciseActivity::class.java)
+            startActivity(i)
+
+        }
+
     }
 }
